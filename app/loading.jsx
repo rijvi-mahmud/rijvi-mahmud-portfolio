9cc9e6367be5c014ab669d3be73212e81a6be456
rijvi-mahmud <rmahmudd17@gmail.com>
2024-10-10 14:38:39 +0600

    style: added loader for loading state

--- app/loading.jsx
+++ app/loading.jsx
@@ -1,7 +1,10 @@
 export default function Loading() {
-    return (
-        <div className="flex justify-center items-center h-screen">
-            <div className="spinner">spinner ...</div>
-        </div>
-    )
+	return (
+		<div className="flex justify-center items-center h-screen">
+			<div className="fixed top-0 left-0 w-full h-full z-[99999999999] flex items-center justify-center dark-gradient">
+				{/* Loading spinner */}
+				<span className="loader"></span>
+			</div>
+		</div>
+	);
 }
